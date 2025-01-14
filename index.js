fc754ab28569e1c9a307e9c8cf586b296715b0a6
abhishekjain97 <68418262+abhishekjain97@users.noreply.github.com>
2025-01-14 18:05:41 +0530

    Update index.js
    Remove devider

--- index.js
+++ index.js
@@ -138,13 +138,10 @@ function createTikcet(ticketId, ticketContent, ticketColor) {
     const priorityTaskCont = document.querySelector("."+ticketColor+"-tasks > .task-body")
 
     const taskItem = document.createElement("div")
-    const taskDevider = document.createElement("div")
     taskItem.setAttribute("class", "task-item")
     taskItem.setAttribute("draggable", "true")
     taskItem.setAttribute("ondragstart", "drag(event)")
     taskItem.setAttribute("id", ""+ticketId)
-
-    taskDevider.setAttribute("class", "task-devider")
 
     taskItem.innerHTML = `<h4>Task Id - ${ticketId}</h4>
                 <p>${ticketContent}</p>
@@ -152,7 +149,6 @@ function createTikcet(ticketId, ticketContent, ticketColor) {
                 <i class="fa-solid fa-lock task_lock_unlock_icon"></i>`
                 
     priorityTaskCont.appendChild(taskItem)
-    priorityTaskCont.appendChild(taskDevider)
     
 
     textArea.value = ""
